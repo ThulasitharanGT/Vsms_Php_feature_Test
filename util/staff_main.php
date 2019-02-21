fa8bc13258e0c72134eb8b64f7d66bea652561ef
ThulasitharanGT <46104397+ThulasitharanGT@users.noreply.github.com>
2019-02-21 22:03:20 +0530

    Add files via upload

---
+++ util/staff_main.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_SESSION['username'])){
+	header("location:./Login_Register/Error.php");
+}
+?>
+<html>
+<head>
+<title> Vehicle Showroom Management System</title><link rel="stylesheet" href="../util/vsms_theme.css">
+
+</head>
+<body bgcolor="Yellow">
+<br><br>
+<center><h1>Vehicle Showroom Management System</h1></center><br>
+<center> <table cellpadding=5 cellspacing=5 border=5>
+<tr>
+<td><a href="../BranchManagement/BranchManagement.php">Branch Management</a></td>
+<td><a href="../CustomerManagement/CustomerManagement.php">Customer Management</a></td>
+<td><a href="../ShowroomManagement/ShowroomManagement.php">Showroom Management</a></td>
+</tr>
+</table>
+<br><br><br><br><br><br><br><br><br>
+<a href="index.php"> click here to go to the home page </a>
+</table> </center>
+</body>
+</html>
